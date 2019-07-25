e8eed22eb30da26b68133b5261a8bed511e643b4
ssshhh0402 <ssshhh0402@naver.com>
2019-07-25 17:49:56 +0900

    20190725| Python

---
+++ python 수업용/notes/myPackage/web/urls.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+
+def make_url(token, method):
+    return f'https://api.telegram.com/bot{token}/{method}'
+
+def docs():
+    webbrowser.open('https://telegram.com')
+    return True
